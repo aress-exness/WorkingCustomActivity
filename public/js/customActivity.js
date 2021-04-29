54b4af1e916a5c7555971881eb3509c8c1d1d75d
aress-exness <78411334+aress-exness@users.noreply.github.com>
2021-04-29 20:09:03 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -92,9 +92,9 @@ define([
 
         payload['arguments'].execute.inArguments = [{
             "tokens": authTokens,
-            "Title": "{{Contact.Attribute.pushyJourney.PushyAPIDictionary.Title}}",
-		    "Data": "{{Contact.Attribute.pushyJourney.PushyAPIDictionary.Data}}",
-		    "DeviceToken": "{{Contact.Attribute.pushyJourney.PushyAPIDictionary.DeviceToken}}"
+            "Title": "{{PushyAPIDictionary.Title}}",
+		    "Data": "{{PushyAPIDictionary.Data}}",
+		    "DeviceToken": "{{PushyAPIDictionary.DeviceToken}}"
         }];
 	          
         payload['metaData'].isConfigured = true;
